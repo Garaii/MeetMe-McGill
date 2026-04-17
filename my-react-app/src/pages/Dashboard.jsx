439aa3f5699eee12d64572f9f3abd3fcadc699ba
Garaii <ikram.gara@mail.mcgill.ca>
2026-04-16 21:22:23 -0400

    details

--- my-react-app/src/pages/Dashboard.jsx
+++ my-react-app/src/pages/Dashboard.jsx
@@ -1,5 +1,35 @@
 // Dashboard.jsx
 function DashboardPage() {
-  return <div>Dashboard Page</div>
+  return (
+  <div>
+    
+    <div className="form-group">
+    <h2>DashBoard</h2>
+    {/*LISTING TO SEE @MCGILL.CA WHO HAVE ACTIVE SLOTS
+    * SELECT OWNER THEN SEE THEIR AVAILABLE SLOTS
+    * AS A USER YOU CAN BOOK A SLOT
+    * 
+    * SEE THE SLOTS YOU HAVE BOOKED
+    * -DELETE --> SLOT OWNER RECEIVE NOTIFICATION ':mailto'
+    * -> slot goes back to available
+    * - MESSAGE THE OWNER OF THE SLOT
+    * 
+    * Users see a dashboard listing their appointments and providing the following options:
+    ▪ They can email (mailto:) the owner of a slot (selected or open slot)
+    ▪ They can delete appointments.
+    ▪ They can book additional appointments.
+    ▪ They can logout
+    * 
+    * 
+    */}
+    <p>WEEK </p>
+    </div>
+    
+    
+    Dashboard Page
+  
+  </div>
+    
+  )
 }
 export default DashboardPage
